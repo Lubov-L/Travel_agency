d9bc6c839641f7dca85d46e696289ba598e2de5e
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-03-18 22:50:14 +0300

    #2 main slider

--- src/components/UI/Main/Slider/Slider.jsx
+++ src/components/UI/Main/Slider/Slider.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import cl from "./slider.module.css";
 
+import {Mousewheel, Keyboard} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const Slider = () => {
     return (
         <div className={cl.slider}>
-
+            <Swiper className=""
+                    cssMode={true}
+                    mousewheel={true}
+                    loop={true}
+                    modules={[Mousewheel, Keyboard]}
+            >
+                <SwiperSlide className={cl.slider__img}>
+                    <img src="/images/slider1.jpg" alt="Slider"></img>
+                </SwiperSlide>
+                <SwiperSlide className={cl.slider__img}>
+                    <img src="/images/slider2.jpg" alt="Slider"></img>
+                </SwiperSlide>
+                <SwiperSlide className={cl.slider__img}>
+                    <img src="/images/slider3.jpg" alt="Slider"></img>
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
