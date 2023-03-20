d488bdf1fe89c8d311ef17310018b356beb4e562
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-03-20 22:53:49 +0300

    slider edit

--- src/components/UI/Main/Slider/Slider.jsx
+++ src/components/UI/Main/Slider/Slider.jsx
@@ -11,37 +11,30 @@ import 'swiper/css/scrollbar';
 import 'swiper/css/effect-fade';
 
 const Slider = () => {
-    return (
-        <div className={cl.slider}>
-            <Swiper
-                cssMode={true}
-                navigation={false}
-                loop={true}
-                mousewheel={true}
-                speed={6000}
-                autoplay={{
-                    delay: 5000,
-                    disableOnInteraction: false
-                }}
-                effect={'fade'}
-                fadeEffect={{
-                    crossFade: true
-                }}
-                modules={[Mousewheel, Keyboard, Autoplay, EffectFade]}
-            >
-                <SwiperSlide className={cl.slider__img}>
-                    <img src="/images/slider1.jpg" alt="Slider"></img>
-                </SwiperSlide>
-                <SwiperSlide className={cl.slider__img}>
-                    <img src="/images/slider2.jpg" alt="Slider"></img>
-                </SwiperSlide>
-                <SwiperSlide className={cl.slider__img}>
-                    <img src="/images/slider3.jpg" alt="Slider"></img>
-                </SwiperSlide>
-            </Swiper>
-        </div>
-    )
-        ;
+    return (<div className={cl.slider}>
+        <Swiper
+            cssMode={true}
+            navigation={false}
+            loop={true}
+            mousewheel={true}
+            autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+            }}
+            speed={1500}
+            modules={[Mousewheel, Keyboard, Autoplay, EffectFade]}
+        >
+            <SwiperSlide className={cl.slider__img}>
+                <img src="/images/slider1.jpg" alt="Slider"></img>
+            </SwiperSlide>
+            <SwiperSlide className={cl.slider__img}>
+                <img src="/images/slider2.jpg" alt="Slider"></img>
+            </SwiperSlide>
+            <SwiperSlide className={cl.slider__img}>
+                <img src="/images/slider3.jpg" alt="Slider"></img>
+            </SwiperSlide>
+        </Swiper>
+    </div>);
 };
 
 export default Slider;
