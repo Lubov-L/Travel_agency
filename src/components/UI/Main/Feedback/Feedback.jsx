e6ae1f31345540ff0cc518a3bb89010a52d8fcf1
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-04-05 22:34:43 +0300

    #3 finish feedback slider

--- src/components/UI/Main/Feedback/Feedback.jsx
+++ src/components/UI/Main/Feedback/Feedback.jsx
@@ -35,13 +35,13 @@ class Feedback extends React.Component {
             <div className="feedback">
                 <h2>Feedbacks from our travelers</h2>
                 <Swiper
-                    effect={"coverflow"}
                     loop={true}
+                    effect={"coverflow"}
                     grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={"auto"}
+                    slidesPerView={3}
                     navigation={true}
                     initialSlide={3}
+                    centeredSlides={true}
                     coverflowEffect={{
                         rotate: 50,
                         stretch: 0,
@@ -59,7 +59,7 @@ class Feedback extends React.Component {
                                     <h2>{feedback.heading}</h2>
                                     <h3>{feedback.name}</h3>
                                     <span>{feedback.date}</span>
-                                    <p>{feedback.text}</p>
+                                    <p className="feedback__text">{feedback.text}</p>
                                 </div>
                             </SwiperSlide>
                         ))}
