837ac13ceb0ab36fc816505fca0d9c00f0110e20
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-04-08 18:51:08 +0300

    #3 edit main information widgets

--- src/components/UI/Main/MainPage.jsx
+++ src/components/UI/Main/MainPage.jsx
@@ -9,10 +9,12 @@ const MainPage = () => {
     return (
         <div className="main__page">
             <Slider>
-                <div className="main__div__blocks">
-                    <MainInf>Millions of people trust us around the world</MainInf>
-                    <MainInf>Travel a lot and make happy memories forever </MainInf>
-                    <MainInf>A lot of sales and discounts for our clients</MainInf>
+                <div className="main__inf__block">
+                    <div className="main__div__blocks">
+                        <MainInf>Millions of people trust us around the world</MainInf>
+                        <MainInf>Travel a lot and make happy memories forever </MainInf>
+                        <MainInf>A lot of sales and discounts for our clients</MainInf>
+                    </div>
                 </div>
             </Slider>
             <ToursList/>
